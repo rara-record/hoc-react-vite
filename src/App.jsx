d7d0a3dfd75a20a99822ab8fb3d97e00133012a9
BORA <a52447879@gmail.com>
2022-12-04 21:08:36 +0900

    feat: render users

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import UserList from './UserList';
 const App = () => {
   return (
     <div className='App'>
+      <h1>Higher Order Component </h1>
       <UserList />
     </div>
   );
--- src/UserList.jsx
+++ src/UserList.jsx
@@ -2,18 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [trem, seTtrem] = useState(' ');
 
   useEffect(() => {
     const fetchUsers = async () => {
       const res = await fetch('https://jsonplaceholder.typicode.com/users');
       const json = await res.json();
       setUsers(json);
-      console.log(users);
     };
     fetchUsers();
   }, []);
 
-  return <div>UserList</div>;
+  let renderUsers = users.map((user) => (
+    <div key={user.id}>
+      <p>
+        <strong>{user.name}</strong>
+      </p>
+    </div>
+  ));
+
+  return (
+    <div>
+      <input
+        text='text'
+        value={trem}
+        onChange={(e) => seTtrem(e.target.value)}
+      />
+      <div>{renderUsers}</div>
+    </div>
+  );
 };
 
 export default UserList;
