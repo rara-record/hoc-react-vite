726f42bc51b8417e7afbccded37b7bf9cf62583a
BORA <a52447879@gmail.com>
2022-12-04 21:32:38 +0900

    feat: add hoc

---
+++ src/HOC.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HOC = (WrapperComponents, entity) => {
+  return class extends Component {
+    state ={}
+    componentsDidMount() {}
+    render()
+  }
+};
+
+export default HOC;
