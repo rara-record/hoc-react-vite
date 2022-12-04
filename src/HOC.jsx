74a475633eab3bfc444878e8a0c3ace852c8575b
BORA <a52447879@gmail.com>
2022-12-04 21:36:22 +0900

    feat: fetch 로직 작성

--- src/HOC.jsx
+++ src/HOC.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 
 const HOC = (WrapperComponents, entity) => {
   return class extends Component {
-    state ={}
-    componentsDidMount() {}
-    render()
-  }
+    state = {
+      data: [],
+      term: '',
+    };
+
+    componentsDidMount() {
+      const fetchUsers = async () => {
+        const res = await fetch(
+          `https://jsonplaceholder.typicode.com/${entity}`
+        );
+        const json = await res.json();
+        this.state = { ...this.state, data: json };
+      };
+      fetchUsers();
+    }
+
+    render() {
+      return <WrapperComponents />
+
+    }
+  };
 };
 
 export default HOC;
